58c62b905ca3711826b7014e72896ae790a347dd
handcopper97 <61685321+handcopper97@users.noreply.github.com>
2022-08-16 14:44:15 +0900

    01. Console 프로젝트 생성
    생성 및 임시파일 삭제

---
+++ Atents_22.08.16_01_Console_2/Atents_22.08.16_01_Console_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atents_22._08._16_01_Console_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
