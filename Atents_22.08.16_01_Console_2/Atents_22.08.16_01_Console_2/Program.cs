6e05debc9951f6487b48df4c3eac6643ec9bfd7c
handcopper97 <61685321+handcopper97@users.noreply.github.com>
2022-08-17 10:11:36 +0900

    Update Program.cs

--- Atents_22.08.16_01_Console_2/Atents_22.08.16_01_Console_2/Program.cs
+++ Atents_22.08.16_01_Console_2/Atents_22.08.16_01_Console_2/Program.cs
@@ -11,6 +11,13 @@ namespace Atents_22._08._16_01_Console_2
         static void Main(string[] args)
         {
             Console.WriteLine("손동욱");//내 이름 출력
+
+            int level = 1, hp = 10;
+            float exp = 0.9f;
+            string name = "너굴맨";
+            Console.WriteLine($"{name}의 레벨은 {level}이고 HP는 {hp}이고 exp는 {exp}다.");
+
+
             Console.ReadKey();
         }
     }
