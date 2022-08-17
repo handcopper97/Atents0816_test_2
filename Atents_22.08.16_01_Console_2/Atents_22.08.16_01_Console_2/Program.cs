8eb700dc65a87bc2fa565ac811e78a30ca22d790
handcopper97 <61685321+handcopper97@users.noreply.github.com>
2022-08-17 10:51:58 +0900

    Update Program.cs

--- Atents_22.08.16_01_Console_2/Atents_22.08.16_01_Console_2/Program.cs
+++ Atents_22.08.16_01_Console_2/Atents_22.08.16_01_Console_2/Program.cs
@@ -13,9 +13,29 @@ namespace Atents_22._08._16_01_Console_2
             Console.WriteLine("손동욱");//내 이름 출력
 
             int level = 1, hp = 10;
-            float exp = 0.9f;
+            float exp = 90;
             string name = "너굴맨";
-            Console.WriteLine($"{name}의 레벨은 {level}이고 HP는 {hp}이고 exp는 {exp}다.");
+            Console.WriteLine($"{name}의 레벨은 {level}이고 HP는 {hp}이고 exp는 {exp}%다.\n");
+
+            string result = $"이름 : {name}\n레벨 : {level}\nHP : {hp}\nexp : {exp}%\n\n";
+            Console.WriteLine(result);
+
+
+            Console.Write("이름을 입력하세요 : ");
+            name = Console.ReadLine();
+
+            Console.Write($"{name}의 레벨을 입력하세요 : ");
+            level = int.Parse(Console.ReadLine());
+
+            Console.Write($"{name}의 hp를 입력하세요 : ");
+            hp = int.Parse(Console.ReadLine());
+
+            Console.Write($"{name}의 경험치를 입력하세요 : ");
+            exp = float.Parse(Console.ReadLine());
+
+            result = $"\n이름 : {name}\n레벨 : {level}\nHP : {hp}\nexp : {exp*100:F3}%\n\n";
+            Console.WriteLine(result);
+
 
 
             Console.ReadKey();
