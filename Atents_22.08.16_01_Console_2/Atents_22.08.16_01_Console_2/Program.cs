33fbbf6bb76ea5ac13acca3129601274a71263ef
handcopper97 <61685321+handcopper97@users.noreply.github.com>
2022-08-18 15:50:41 +0900

    전투
    한 쪽이 죽을 때 까지(hp가 0 이하가 될 때까지) 싸우기

--- Atents_22.08.16_01_Console_2/Atents_22.08.16_01_Console_2/Program.cs
+++ Atents_22.08.16_01_Console_2/Atents_22.08.16_01_Console_2/Program.cs
@@ -10,86 +10,11 @@ namespace Atents_22._08._16_01_Console_2
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("손동욱");//내 이름 출력
-
-            //int level, hp, maxHP;
-            //float exp, maxExp;
-            //string name;
-
-            //Console.WriteLine($"{name}의 레벨은 {level}이고 HP는 {hp}이고 exp는 {exp}%다.\n");
-
-            //string result = $"이름 : {name}\n레벨 : {level}\nHP : {hp}\nexp : {exp}%\n\n";
-            //Console.WriteLine(result);
-
-
-            //Console.Write("이름을 입력하세요 : ");
-            //name = Console.ReadLine();
-
-            //Console.Write($"{name}의 레벨을 입력하세요 : ");
-            //level = int.Parse(Console.ReadLine());
-
-            //Console.Write($"{name}의 hp를 입력하세요 : ");
-            //hp = int.Parse(Console.ReadLine());
-
-            //Console.Write($"{name}의 maxHp를 입력하세요 : ");
-            //maxHP = int.Parse(Console.ReadLine());
-
-
-            //Console.Write($"{name}의 경험치를 입력하세요 : ");
-            //exp = float.Parse(Console.ReadLine());
-
-            //Console.Write($"{name}의 max경험치를 입력하세요 : ");
-            //maxExp = float.Parse(Console.ReadLine());
-
-            ////result = $"\n이름 : {name}\n레벨 : {level}\nHP : {hp}\nexp : {exp * 100:F3}%\n\n";
-            ////Console.WriteLine(result);
-
-            //PrintCharacter(name, level, hp, maxHP, exp, maxExp);
-            //Console.WriteLine("경험치를 추가합니다.\n");
-            //Console.Write("추가할 경험치 : ");
-            //string temp = Console.ReadLine();
-            //float sum = exp + float.Parse(temp);
-            //if (sum >= 1)
-            //{
-            //    Console.WriteLine("\n레벨업");
-            //}
-            //else
-            //{
-
-
-            //    Console.WriteLine($"\n현재 경험치는 {sum}입니다.\n\n\n");
-            //}
-
-            //exp = 0.0f;
-            //sum = 0;
-            //Console.WriteLine($"\n반복문 시작.\n현재 경험치는 {sum}입니다.");
-            //while (sum < 1)
-            //{
-            //    Console.Write("추가할 경험치를 입력해주세요 : ");
-            //    temp = Console.ReadLine();
-
-            //    sum += exp + float.Parse(temp);
-
-            //    Console.WriteLine($"\n현재 경험치는 {sum}입니다.");
-            //}
-
-            //while (sum > 1)
-            //{
-            //    sum -= 1;
-            //    Console.WriteLine($"\n레벨업\n현재 경험치는 {sum * 100:f1}%입니다.");
-            //}
-            //int temp;
-            //temp = calcul();
-
-
-            //print(temp);
-
             Character human1 = new Character();
+            
+            Character human2 = new Character("테스트용");
 
-
-
-            //human1.name = "a";
-
+            human1.Fight(human2);
 
             Console.ReadKey();
         }
@@ -121,15 +46,5 @@ namespace Atents_22._08._16_01_Console_2
             }
             return a;
         }
-
-        //private static void PrintCharacter(string name, int level, int hp, int maxHP, float exp, float maxExp)
-        //{
-           
-        //    string result = $"\n이름 : {name}\n레벨 : {level}\nHP/maxHP : {hp}/{maxHP}\nexp/maxExp : {exp * 100:F3}%/{maxExp*100:F3}\n\n";
-
-        //    Console.WriteLine(result);
-
-
-        //}
     }
 }
--- Atents_22.08.16_01_Console_2/Atents_22.08.16_01_Console_2/test.cs
+++ Atents_22.08.16_01_Console_2/Atents_22.08.16_01_Console_2/test.cs
@@ -6,73 +6,94 @@ using System.Threading.Tasks;
 
 namespace Atents_22._08._16_01_Console_2
 {
+
     public class Character
     {
         private string name;
-        private int hp = 100, maxHP = 100, strenth = 10, dexterity = 5, Intellegence = 7;
-
+        private int hp = 100, maxHP = 100, strenth = 10, dexterity = 5, Intellegence = 7, randNum = 0;
         string[] nameArray = { "1번", "2번", "3번", "4번", "5번" };
+        Random random;
 
-        public Character()
+        public Character(string name)
         {
-            Random random = new Random();
-            int randomNum = random.Next();
-            name = nameArray[randomNum % 4];
-            resetRendom(randomNum, random);
-            while (randomNum % 201 < 100)
-            {
-                resetRendom(randomNum, random);
-            }
-            maxHP = randomNum % 201;
-            hp = maxHP;
+            random = new Random();
 
-
-            randomNum = random.Next();
-            while (true)
-            {
-                strenth = randomNum % 21;
-                randomNum = random.Next();
-                if (strenth != 0)
-                {
-                    break;
-                }
-            }
-            while (true)
-            {
-                randomNum = random.Next();
-                dexterity = randomNum % 21;
-                if (dexterity != 0)
-                {
-                    break;
-                }
-            } while (true)
-            {
-                randomNum = random.Next();
-                Intellegence = randomNum % 21;
-                if (Intellegence != 0)
-                {
-                    break;
-                }
-            }
+            Genarate();
+            this.name = name;
 
             TestPrintStatus(this);
+        }
+        public Character()
+        {
+            random = new Random();
+            randNum = random.Next(5);
+            name = nameArray[randNum];
+            Genarate();
+
+            TestPrintStatus(this);
+        }
+
+        void Genarate()
+        {
+
+            maxHP = random.Next(100, 201);
+            hp = maxHP;
+            strenth = random.Next(20) + 1;
+            dexterity = random.Next(20) + 1;
+            Intellegence = random.Next(20) + 1;
+
         }
         void TestPrintStatus(Character c)
         {
             Console.WriteLine($"이름\t:\t{c.name}\nhp\t:\t{c.hp}/{c.maxHP}\n힘\t:\t{c.strenth}\n민첩\t:\t{c.dexterity}\n지능\t:\t{c.Intellegence}\n");
         }
 
-        void resetRendom(int r, Random rr)
+        public void Attack(Character target)
         {
-            r = rr.Next();
+            int damage = strenth;
+            Console.WriteLine($"{name}이 {target.name}에게 피해를 {damage}만큼 가했습니다.\n");
+            target.TakeDamage(damage, this);
+        }
+        public void TakeDamage(int d, Character attacker)
+        {
+            TestPrintStatus(attacker);
+            TestPrintStatus(this);
+            hp -= d;
+            Console.WriteLine($"\n{name}의 체력은 {d}만큼의 피해를 입었습니다.\n현재 {name}의 체력은 {hp + d}에서 {hp}로 변경\n");
+            if (hp <= 0)
+            {
+                Console.WriteLine("전투가 종료되었습니다.");
+                Console.WriteLine($"{name}은 사망하였습니다.");
+            }
+
+
         }
 
-        //public Character(string newName)
-        //{
-        //    name = newName;
-        //}
+        public void Fight(Character target)
+        {
+            bool die = false;
+            while (!die)
+            {
+                Attack(target);
+                die = HP(this,target);
+
+                target.Attack(this);
+                die = HP(this, target);
+            }
+        }
+
+        bool HP(Character a, Character b)
+        {
+            if (a.hp <= 0 || b.hp <= 0)
+            {
+                
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
-    internal class test
-    {
-    }
+
 }
