ffb0af4f6dec9166d6198af120c26626c70197c6
handcopper97 <61685321+handcopper97@users.noreply.github.com>
2022-08-23 14:14:51 +0900

    실습

--- Unity_실습/Atents/Assets/Move.cs
+++ Unity_실습/Atents/Assets/Move.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    public float speed=1;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    //°¡³ª´Ù
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.right;
+        transform.position +=(speed * Time.deltaTime * Vector3.right);
     }
 }
