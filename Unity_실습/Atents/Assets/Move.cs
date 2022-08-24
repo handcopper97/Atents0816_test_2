449e458f185d64e90287ec2d647a2bfa08662d34
handcopper97 <61685321+handcopper97@users.noreply.github.com>
2022-08-24 12:50:26 +0900

    인풋 시스템 실습

--- Unity_실습/Atents/Assets/Move.cs
+++ Unity_실습/Atents/Assets/Move.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Move : MonoBehaviour
 {
@@ -8,23 +9,35 @@ public class Move : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("시작");
     }
     //가나다
     // Update is called once per frame
     void Update()
     {
-        //transform.position +=(speed * Time.deltaTime * Vector3.right);
+        //Move_Key();
+    }
+    public void MoveInput(InputAction.CallbackContext context)
+    {
+        Debug.Log("입력들어옴");
+    }
+
+    private static void Move_Key()
+    {
+        ////transform.position +=(speed * Time.deltaTime * Vector3.right);
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             Debug.Log("오른쪽 화살표가 눌렸습니다.");
-        } else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             Debug.Log("왼쪽 화살표가 눌렸습니다.");
-        } else if (Input.GetKeyDown(KeyCode.UpArrow))
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             Debug.Log("위쪽 화살표가 눌렸습니다.");
-        } else if (Input.GetKeyDown(KeyCode.DownArrow))
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             Debug.Log("아래쪽 화살표가 눌렸습니다.");
         }
