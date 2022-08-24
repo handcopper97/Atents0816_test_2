26f1ea134ee890e0ffda7784a73b1f631405a8dc
handcopper97 <61685321+handcopper97@users.noreply.github.com>
2022-08-24 13:53:16 +0900

    실습(발사, 무빙)
    인풋 시스템 이용

--- Unity_실습/Atents/Assets/Move.cs
+++ Unity_실습/Atents/Assets/Move.cs
@@ -5,6 +5,8 @@ using UnityEngine.InputSystem;
 
 public class Move : MonoBehaviour
 {
+    Vector2 inputDir;
+    public bool ing = false;
     public float speed=1;
     // Start is called before the first frame update
     void Start()
@@ -15,11 +17,47 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (ing)
+        {
+            transform.position += new Vector3(inputDir.x, inputDir.y, 0)*Time.deltaTime;
+        }
         //Move_Key();
     }
     public void MoveInput(InputAction.CallbackContext context)
     {
-        Debug.Log("입력들어옴");
+        
+        inputDir = context.ReadValue<Vector2>();
+        if (context.started)
+        {
+            Debug.Log("입력값 인식");
+        }
+        else if (context.performed)
+        {
+            transform.position += new Vector3(inputDir.x, inputDir.y, 0)*Time.deltaTime;
+            ing = true;
+            Debug.Log("버튼 눌림");
+
+        }
+        else if (context.canceled)
+        {
+            ing = false;
+            Debug.Log("버튼 땜");
+        }
+        else
+        {
+
+        }
+        
+       
+        Debug.Log(inputDir);
+
+    }
+    public void Shot(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            Debug.Log("발사");
+        }
     }
 
     private static void Move_Key()
@@ -42,4 +80,11 @@ public class Move : MonoBehaviour
             Debug.Log("아래쪽 화살표가 눌렸습니다.");
         }
     }
+    /*
+     * 실습0824    
+     * 1. 인풋 시스템을 이용해서  wasd로 이동한다.
+     *  1.1 키를 누르고 있으면 땔 때까지 해당 키 방향으로 계속 이동한다.
+     * 2. 스페이스 키를 누르면 콘솔창에 "발사" 텍스트가 출력
+     * 14:20 까지
+     */
 }
